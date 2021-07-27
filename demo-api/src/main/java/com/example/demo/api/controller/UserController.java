b748499e6eaa255562211ff7e230a98e426625ce
HAIER\01439090 <qdtantan@126.com>
2021-07-27 12:08:16 +0800

    knife4j配置

--- demo-api/src/main/java/com/example/demo/api/controller/UserController.java
+++ demo-api/src/main/java/com/example/demo/api/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.example.demo.api.controller;
 
 import com.example.demo.core.user.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(tags = "首页模块")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
+    @ApiOperation(value = "用户详情")
     @GetMapping("/detail")
     public String accountDetail(){
         return "liweidetail";
